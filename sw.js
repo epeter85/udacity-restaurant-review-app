bbf20ac7ace30f06689a5fd2840f3eedafda9007
Erik Peterson <erik@erik-id.com>
2019-02-07 18:52:34 -0500

    added service worker

---
+++ sw.js
@@ -0,0 +1,71 @@
+const filesToCache = [
+  '/',
+  'css/responsive.css',
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'index.html',
+  'offline.html',
+  '404.html'
+];
+
+const staticCacheName = 'pages-cache-v4';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+      .then(response => {
+        if (response.status === 404) {
+          return caches.match('404.html');
+        }
+        return caches.open(staticCacheName)
+        .then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+    }).catch(error => {
+      console.log('Error, ', error);
+      return caches.match('offline.html');
+    })
+  );
+});
